80ffba789c205ebf8648d036d240e5f86e63831c
Thiago Flausino <thiago.flausino@kaefer.com>
2026-09-04 18:19:19 -0300

    Adicionando Casos de Uso, Atualizando Migrations e README

--- OrexApp/Features/ManterAuth/Login/DTOs/RefreshTokenRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace OrexApp.Features.ManterAuth.Login.DTOs.RefreshTokenRequest
-{
-    public record RefreshTokenRequest
-    {
-        
-    }
-}
